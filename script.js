66d622db9c9b4114d90342f14e9d3241b766438a
HUMURA ELVIN <humura@humura>
2024-04-11 11:31:04 +0200

    Uploaded Files

---
+++ script.js
@@ -0,0 +1,43 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", ()=>{
+   let inpWord = document.getElementById("inp-word").value;
+   console.log(inpWord); 
+
+   fetch(`${url}${inpWord}`)
+   .then((response) => response.json())
+   .then((data) => {
+    console.log(data);
+    result.innerHTML = `
+    <div class="word">
+                    <h3>${inpWord}</h3>
+                    <button onClick ="playSound()">
+                        <img src="./sound.png" class="fas fa-volume-up">
+                    </button>
+                </div>
+                <div class="details">
+                    <p>${data[0].meanings[0].partsOfSpeech}</p>
+                    <p>/${data[0].phonetic}/</p>
+                </div>
+                <p class="word-meaning">
+                    ${data[0].meanings[0].definitions[0].definiton}
+                </p>
+                <p class="word-example">
+                    ${data[0].meanings[0].definitions[0].example || ""}
+                </p>`;
+
+                sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
+   })
+
+   .catch(()=> {
+      result.innerHTML = `<h3 class = "error">Couldn't Find The Word</h3>`;
+   });
+});
+
+function playSound(){
+    sound.play();
+}
